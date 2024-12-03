13b34547e46fb13bc5ccf84a02652daf8a8cb390
Pedro Hurtado <pedrohurt@gmail.com>
2024-12-03 13:58:22 +0100

    add projections link

--- src/main/java/com/example/demo/infraestructure/IngredientJpa.java
+++ src/main/java/com/example/demo/infraestructure/IngredientJpa.java
@@ -3,6 +3,7 @@ package com.example.demo.infraestructure;
 
 import java.util.UUID;
 
+//https://www.baeldung.com/spring-data-jpa-projections
 public interface IngredientJpa {
 
     public UUID getId();
