7697b21b28a8d2631606100cb5c7f717d5bd0ab7
Pedro Hurtado <pedrohurt@gmail.com>
2024-12-03 11:18:41 +0100

    implement functional interface in remove ingredients

--- src/main/java/com/example/demo/controller/ingredient/commands/IngredientRemove.java
+++ src/main/java/com/example/demo/controller/ingredient/commands/IngredientRemove.java
@@ -4,7 +4,6 @@ import java.util.UUID;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,39 +14,27 @@ import com.example.demo.domain.Ingredient;
 @Configuration
 public class IngredientRemove {
 
+    private interface Service {
+        void handler(UUID id);
+    }
 
     @RestController
     private class Controller {
-    
-        private final Service service;
+
+        private final Service service;      
         @SuppressWarnings("unused")
-        public Controller(final Service service){
-            this.service = service;
+        public Controller(Remove<Ingredient, UUID> respository) {
+            this.service = id -> {
+                Ingredient ingredient = respository.get(id);
+                respository.remove(ingredient);
+            };
         }
+
         @DeleteMapping("/ingredients/{id}")
-        public ResponseEntity<?> handler(@PathVariable UUID id){
+        public ResponseEntity<?> handler(@PathVariable UUID id) {
             service.handler(id);
             return ResponseEntity.noContent().build();
         }
-        
-    }
 
-    private interface  Service {
-        void handler(UUID id);        
-    }
-    @Component
-    private class ServiceImpl implements Service{
-
-        private final Remove<Ingredient,UUID> respository;
-        @SuppressWarnings("unused")
-        public ServiceImpl(final Remove<Ingredient,UUID> respository){
-            this.respository = respository;
-        }
-        @Override
-        public void handler(UUID id) {
-            Ingredient ingredient = respository.get(id);
-            respository.remove(ingredient);
-        }
-
-    }
+    }   
 }
